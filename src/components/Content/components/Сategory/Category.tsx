5c3a60bf5beeb4a491fc975087dd8fd8d2850c7a
Kuzin Igor <igor1utsu@gmail.com>
2022-12-02 04:23:30 +0300

    Рефакторинг данных, добавил фильтр для RepairParts

--- src/components/Content/components/Сategory/Category.tsx
+++ src/components/Content/components/Сategory/Category.tsx
@@ -2,33 +2,84 @@ import "./Category.scss"
 import { Button, Image, InputNumber, Table } from "antd"
 import type { ColumnsType } from "antd/es/table"
 import { ShoppingCartOutlined } from "@ant-design/icons"
-import RepairPatrs from "../../../../data/repairParts.json"
+import REPAIR_PARTS from "../../../../data/REPAIR_PARTS.json"
 
-export const Category = () => {
-  interface RepairPartsType {
-    id: string
-    name: string
-    number: string
-    category: string
+interface CategoryProps {
+  category: string
+  title: string
+}
+
+interface DataRepairPartsType {
+  id: number
+  name: string
+  company: string
+  category: string
+  make: {
+    repairPartID: number
+    brand: string
     models: {
-      make: string
+      repairPartID: number
       model: string
-      year: number[]
+      series: {
+        id: number
+        repairPartID: number
+        number: string
+        price: number
+        image: string
+        year: number[]
+      }[]
     }[]
-    image: string
-    price: number
-  }
+  }[]
+}
 
-  console.log(RepairPatrs)
+interface RepairPartsType {
+  name: string | undefined
+  id?: number | undefined
+  repairPartID?: number | undefined
+  number?: string | undefined
+  price?: number | undefined
+  image?: string | undefined
+  year?: number[] | undefined
+}
+
+export const Category = ({ category, title }: CategoryProps) => {
+  console.log([category, title])
+
+  // console.log(RepairPatrs)
+  const selectedMake: string = "honda" // выбираем марку
+  const selectedModel: string = "CR250R" // выбираем модель
+  const selectedYear: number = 1993 // выбираем год
+
+  const filterByCategory = REPAIR_PARTS.filter(
+    (rp: DataRepairPartsType) => rp.category === category
+  )
+  console.log(category, filterByCategory)
+  const findByMake = filterByCategory.map((rp) =>
+    rp.make.find((res) => res.brand === selectedMake)
+  )
+  console.log("Find.MAKE:", findByMake)
+  const findByModel = findByMake.map((rp) =>
+    rp?.models.find((res) => res.model === selectedModel)
+  )
+  console.log("Find.MODEL:", findByModel)
+  const findByYear = findByModel.map((rp) =>
+    rp?.series.find((res) => res.year.includes(selectedYear))
+  )
+  console.log("Find.YEAR", findByYear)
+  const repairPartsArray = findByYear
+    .filter((res) => res != null)
+    .map((rp) => ({
+      ...rp,
+      name: REPAIR_PARTS.find((RP) => RP.id === rp?.repairPartID)?.name,
+    }))
+  console.log("RP_Array:", repairPartsArray)
 
   const columns: ColumnsType<RepairPartsType> = [
     {
       title: "Image",
       dataIndex: "image",
       key: "image",
-      render: (image) => (
-        <Image src={"/img/repair-parts/" + image}/>
-      ),
+      render: (image) => <Image src={"/img/repair-parts/" + image} />,
       width: 64,
       align: "center",
     },
@@ -70,10 +121,10 @@ export const Category = () => {
 
   return (
     <div className="category">
-      <div className="category__header">Engine</div>
+      <div className="category__header">{title}</div>
       <Table
         columns={columns}
-        dataSource={RepairPatrs}
+        dataSource={repairPartsArray}
         pagination={false}
         // showHeader={false}
       />
