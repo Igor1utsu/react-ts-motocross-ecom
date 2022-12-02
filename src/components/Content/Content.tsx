5ad19820e39b1cd625e253763445713f4823ba37
Kuzin Igor <igor1utsu@gmail.com>
2022-12-02 03:59:22 +0300

    Добавил рендеринг таблицы по категориям

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,12 +1,19 @@
 import { Category } from "./components/Сategory/Category"
 import "./Content.scss"
+import CATEGORIES from "../../data/CATEGORIES.json"
+
+interface CategoriesType {
+  id: number
+  category: string
+  title: string
+}
 
 export const Content = () => {
   return (
     <div className="content">
-      <Category />
-      <Category />
-      <Category />
+      {CATEGORIES.map((data) => (
+        <Category category={data.category} title={data.title} key={data.id} />
+      ))}
     </div>
   )
 }
