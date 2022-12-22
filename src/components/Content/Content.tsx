6159d18ccf5cf850a63b3e5c1ab6db305d8716ec
Kuzin Igor <igor1utsu@gmail.com>
2022-12-22 11:10:08 +0300

    Рефакторинг Content + добавил дынные о продуктах

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,28 +1,39 @@
 import "./Content.scss"
-import { useNavigate } from "react-router-dom"
-import PartsLogo from "../../assets/img/Dirt-Bike-Parts.jpg"
+import { Link } from "react-router-dom"
+import CONTENT from "../../data/CONTENT.json"
+import { Card } from "antd"
+import { PATH_TO_PICTURE } from "../../data/data"
 
 export const Content = () => {
-  const history = useNavigate()
-
   return (
     <div className="content">
-      <div className="products" onClick={() => history("parts")}>
-        <div className="products__item">
-          <h4 className="item__title">Parts:</h4>
-          <div className="item__content body">
-            <div className="body__logo">
-              <img src={PartsLogo} alt="Parts" />
-            </div>
-            <div className="body__text">
-              <span className="body__text__item">Honda</span>
-              <span className="body__text__item">Kawasaki</span>
-              <span className="body__text__item">KTM</span>
-              <span className="body__text__item">Suzuki</span>
-              <span className="body__text__item">Yamaha</span>
-            </div>
-          </div>
-        </div>
+      <div className="products">
+        {CONTENT.map((data, index) => (
+          <Link to={data.name} key={index}>
+            <Card
+              title={data.title}
+              headStyle={{
+                textAlign: "center",
+                textTransform: "uppercase",
+                fontSize: 18,
+              }}
+              className="products__item"
+            >
+              <img
+                src={PATH_TO_PICTURE.img + data.image}
+                alt={data.title}
+                style={{ width: "100%", marginBottom: 10 }}
+              />
+              <ul className="product__text">
+                {data.text?.map((item, index) => (
+                  <li className="text__item" key={index}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </Card>
+          </Link>
+        ))}
       </div>
     </div>
   )
