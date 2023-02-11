e3c08b2402fe5fd22f94917e366d9ce0ca699224
Kuzin Igor <igor1utsu@gmail.com>
2023-02-11 17:46:13 +0300

    создал хук useGetProductList

--- src/components/Content/components/Category/Category.tsx
+++ src/components/Content/components/Category/Category.tsx
@@ -1,15 +1,14 @@
-import "./Category.scss"
-import { Button, Image, InputNumber, Table } from "antd"
-import type { ColumnsType } from "antd/es/table"
-import { ShoppingCartOutlined } from "@ant-design/icons"
-import { Link } from "react-router-dom"
-import PARTS from "../../../../data/PARTS.json"
-import { IDataParts } from "../../../../shared/model/IDataParts"
-import { PATH_TO_PICTURE } from "../../../../data/data"
-import { useContext, useMemo } from "react"
-import { FilterOptionsContext } from "../../../../context/FilterOptionsContext"
-import { CartContext } from "../../../../context/CartContext"
-import { useNotification } from "../../../../utils/hooks"
+import './Category.scss'
+import { Button, Image, InputNumber, Table } from 'antd'
+import type { ColumnsType } from 'antd/es/table'
+import { ShoppingCartOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import { IDataParts } from '../../../../shared/model/IDataParts'
+import { PATH_TO_PICTURE } from '../../../../data/data'
+import { useContext } from 'react'
+import { FilterOptionsContext } from '../../../../context/FilterOptionsContext'
+import { CartContext } from '../../../../context/CartContext'
+import { useGetProductList, useNotification } from '../../../../utils/hooks'
 
 interface ICategoryProps {
   id: number
@@ -22,84 +21,50 @@ export const Category = ({ id, category, title }: ICategoryProps) => {
     useContext(FilterOptionsContext)
   const { addToCart } = useContext(CartContext)
   const { openNotification, contextHolder } = useNotification()
-
-  // фильтруем данные по категориям
-  const dataByCategory = PARTS.filter((data) => data.category === category)
-
-  // фильтруем данные по выбранным брендам
-  const dataByBrand = useMemo(() => {
-    if (checkedBrand.length === 0) return dataByCategory
-    else
-      return dataByCategory.filter((data) => {
-        let selectBrand = data.brand
-        return (
-          data.brand === checkedBrand.find((checked) => checked === selectBrand)
-        )
-      })
-  }, [checkedBrand, dataByCategory])
-
-  // фильтруем данные по выбранной марке
-  const dataBySelectBike = useMemo(() => {
-    if (make && model && year) {
-      return dataByBrand.filter((data) =>
-        data.fits.find(
-          (bike) =>
-            bike.make === make &&
-            bike.model === model &&
-            bike.year.find((YEAR) => YEAR === year)
-        )
-      )
-    } else return dataByBrand
-  }, [dataByBrand, make, model, year])
-
-  // получаем полные данные о продукте
-  const PartsDataArray = dataBySelectBike
-    // фильтруем товар по минимальной цене
-    .filter((data) => {
-      if (minPrice && data?.price) return data?.price >= minPrice
-      return data
-    })
-    // фильтруем товар по максимальной цене
-    .filter((data) => {
-      if (maxPrice && data?.price) return data?.price <= maxPrice
-      return data
-    })
-    .map((part, index) => ({ ...part, key: index }))
+  const PartsDataArray = useGetProductList(
+    make,
+    model,
+    year,
+    minPrice,
+    maxPrice,
+    category,
+    checkedBrand
+  )
 
   const columns: ColumnsType<IDataParts> = [
     {
-      title: "Image",
-      dataIndex: "image",
-      key: "image",
+      title: 'Image',
+      dataIndex: 'image',
+      key: 'image',
       render: (image) => <Image src={PATH_TO_PICTURE.parts + image} />,
       width: 64,
-      align: "center",
+      align: 'center',
     },
     {
-      title: "Part Name",
-      dataIndex: "name",
-      key: "name",
+      title: 'Part Name',
+      dataIndex: 'name',
+      key: 'name',
       render: (name, data) => (
         <Link to={`${data.partNumber}`}>
           {name}
-          {" for "}
+          {' for '}
           {data.fits.map(
             (data) =>
               data.make +
-              " " +
+              ' ' +
               data.model +
-              " " +
+              ' ' +
               (data.year.length > 2
-                ? data.year[0] + " - " + data.year[data.year.length - 1] + " "
-                : data.year[0] + " " + data.year[1] + " ")
+                ? data.year[0] + ' - ' + data.year[data.year.length - 1] + ' '
+                : data.year[0] + ' ' + data.year[1] + ' ')
           )}
         </Link>
       ),
     },
     {
-      title: "Price",
-      dataIndex: "price",
-      key: "price",
+      title: 'Price',
+      dataIndex: 'price',
+      key: 'price',
       render: (price) => (
         <>
           <span>$</span>
@@ -107,16 +72,16 @@ export const Category = ({ id, category, title }: ICategoryProps) => {
         </>
       ),
       width: 64,
-      align: "center",
+      align: 'center',
     },
     {
-      title: "Buy",
-      dataIndex: "byy",
-      key: "buy",
+      title: 'Buy',
+      dataIndex: 'byy',
+      key: 'buy',
       render: (_, data) => {
         let value: number = 1
         const handleAddToCart = () => {
-          openNotification("bottomRight", data)
+          openNotification('bottomRight', data)
           addToCart(data.id, value)
         }
 
@@ -139,7 +104,7 @@ export const Category = ({ id, category, title }: ICategoryProps) => {
         )
       },
       width: 140,
-      align: "center",
+      align: 'center',
     },
   ]
 
--- src/utils/hooks.tsx
+++ src/utils/hooks.tsx
@@ -1,9 +1,65 @@
-import { Image, notification } from "antd"
-import { NotificationPlacement } from "antd/es/notification/interface"
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { PATH_TO_PICTURE } from "../data/data"
-import { IDataParts } from "../shared/model/IDataParts"
+import { Image, notification } from 'antd'
+import { NotificationPlacement } from 'antd/es/notification/interface'
+import axios from 'axios'
+import { useEffect, useMemo, useState } from 'react'
+import { PATH_TO_PICTURE } from '../data/data'
+import { IDataParts } from '../shared/model/IDataParts'
+import PARTS from '../data/PARTS.json'
+
+export const useGetProductList = (
+  make: string | null,
+  model: string | null,
+  year: number | null,
+  minPrice: number,
+  maxPrice: number,
+  category: string,
+  checkedBrand: string[]
+) => {
+  // фильтруем данные по категориям
+  const dataByCategory = PARTS.filter((data) => data.category === category)
+
+  // фильтруем данные по выбранным брендам
+  const dataByBrand = useMemo(() => {
+    if (!checkedBrand.length) return dataByCategory
+    else
+      return dataByCategory.filter((data) => {
+        let selectBrand = data.brand
+        return (
+          data.brand === checkedBrand.find((checked) => checked === selectBrand)
+        )
+      })
+  }, [checkedBrand, dataByCategory])
+
+  // фильтруем данные по выбранной марке
+  const dataBySelectBike = useMemo(() => {
+    if (make && model && year) {
+      return dataByBrand.filter((data) =>
+        data.fits.find(
+          (bike) =>
+            bike.make === make &&
+            bike.model === model &&
+            bike.year.find((YEAR) => YEAR === year)
+        )
+      )
+    } else return dataByBrand
+  }, [dataByBrand, make, model, year])
+
+  // получаем полные данные о продукте
+  const partsDataArray = dataBySelectBike
+    // фильтруем товар по минимальной цене
+    .filter((data) => {
+      if (minPrice && data?.price) return data?.price >= minPrice
+      return data
+    })
+    // фильтруем товар по максимальной цене
+    .filter((data) => {
+      if (maxPrice && data?.price) return data?.price <= maxPrice
+      return data
+    })
+    .map((part, index) => ({ ...part, key: index }))
+
+  return partsDataArray
+}
 
 export const useNotification = () => {
   const [api, contextHolder] = notification.useNotification()
@@ -24,11 +80,11 @@ export const useNotification = () => {
 }
 
 export const useGetAddress = (coordinates: number[] | undefined) => {
-  const [address, setAddress] = useState("")
+  const [address, setAddress] = useState('')
 
   const getAddress = async (coordinates: number[]) => {
     // преобразуем координаты в строковое значение
-    const geocode = coordinates[1].toString() + "," + coordinates[0].toString()
+    const geocode = coordinates[1].toString() + ',' + coordinates[0].toString()
 
     const url = `https://geocode-maps.yandex.ru/1.x/?format=json&apikey=${process.env.REACT_APP_YANDEX_API_KEY}&geocode=${geocode}&lang=en_US`
 
@@ -40,12 +96,12 @@ export const useGetAddress = (coordinates: number[] | undefined) => {
     // поиск адреса
     const geocoderMetaData = geoObjectList.find(
       (data: any) =>
-        data.GeoObject.metaDataProperty.GeocoderMetaData.kind === "house"
+        data.GeoObject.metaDataProperty.GeocoderMetaData.kind === 'house'
     )
     // преобразование адресса в строку
     const address =
-    (geocoderMetaData.GeoObject.description.split(","))[0] +
-      ", " +
+      geocoderMetaData.GeoObject.description.split(',')[0] +
+      ', ' +
       geocoderMetaData.GeoObject.name
 
     setAddress(address)
