32a54977e16fe5709e1bbb46841210ed2fa4c034
Kuzin Igor <igor1utsu@gmail.com>
2023-03-01 20:15:49 +0300

    Рефакторинг Page Not Found

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,8 +1,9 @@
+import { FC, memo } from "react"
 import { PAGE_404_TITLE } from "../../shared/constants/Page.constants"
 import { usePageTitle } from "../../shared/hooks/usePageTitle"
 
-export const PageNotFound = () => {
+export const PageNotFound: FC = memo(() => {
   usePageTitle(PAGE_404_TITLE)
 
   return <h1>{PAGE_404_TITLE}</h1>
-}
+})
