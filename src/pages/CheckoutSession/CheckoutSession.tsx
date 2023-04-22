6c065474a30a1145398c1962de0e9d6342dd1695
Kuzin Igor <igor1utsu@gmail.com>
2023-04-22 13:44:01 +0300

    Рефакторинг CheckoutSession

--- src/pages/CheckoutSession/CheckoutSession.tsx
+++ src/pages/CheckoutSession/CheckoutSession.tsx
@@ -3,16 +3,17 @@ import { FC, memo } from "react"
 import { GettingPickup } from "../../layouts/CheckoutLayouts/components/GettingPickup/GettingPickup"
 import { UserContactForm } from "../../layouts/CheckoutLayouts/components/UserContactForm/UserContactForm"
 import { ProductsInfo } from "../../layouts/CheckoutLayouts/components/ProductsInfo/ProductsInfo"
+import clsx from "clsx"
 
 export const CheckoutSession: FC = memo(() => {
   return (
-    <div className="container content-wrapper">
+    <main className={clsx(styles["CheckoutSession"], "container")}>
       <h1 className={styles["CheckoutSession__title"]}>Checkout</h1>
       <div className={styles["CheckoutSession__grid"]}>
         <GettingPickup />
         <ProductsInfo />
         <UserContactForm />
       </div>
-    </div>
+    </main>
   )
 })
